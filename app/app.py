17674760bdddaa4e8b4dd49bc0ea8c6e46f48659
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-04-04 01:25:48 +0200

    add logs

--- app/app.py
+++ app/app.py
@@ -9,12 +9,17 @@ time.sleep(2)  # Leave time for the Arduino to initialize
 
 
 def sendMessage(message):
+    print("Sending "+message+" via serial port")
     ser.write((message+"\n").encode('utf-8'))
+    print("Sent")
 
 
 def fetchAvailableEBikes():
+    print("Fetching data")
     data = requests.get('https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=turbigo&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes&refine.name=Turbigo+-+R%C3%A9aumur')
-    return data.json()['records'][0]["fields"]["ebike"]
+    res = data.json()['records'][0]["fields"]["ebike"]
+    print("Received "+str(res))
+    return res
 
 
 if __name__ == '__main__':
